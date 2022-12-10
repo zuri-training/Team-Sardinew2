7d872b65bb0bc3f7745595f3793f74fdac27e181
Kakwiri <97972926+KakwiriStephen@users.noreply.github.com>
2022-12-10 10:56:50 +0300

    Updated Userjs to prompt user upon empty required  Fields

--- backend/src/model/User.js
+++ backend/src/model/User.js
@@ -3,13 +3,13 @@ const {Schema, model} = require("mongoose");
 const userSchema = new Schema({
     full_name:{
         type:String,
-        required:true,
+        required:[true,'Please Enter Your Full Name'],
         minlength:5,
         maxlength:100
     },
     username:{
         type:String,
-        required:true,
+        required:[true,'Please Enter Your Username'],
         unique:true,
         minlength:3,
         maxlength:30
@@ -17,13 +17,13 @@ const userSchema = new Schema({
     email:{
         type:String,
         required:true,
-        unique:true,
+        unique:[true,'Please Enter Your Username'],
         minlength:5,
         maxlength:100
     },
     password:{
         type:String,
-        required:true,
+        required:[true,'Please Enter Your Password'],
         minlength:5,
         maxlength:100
     },
